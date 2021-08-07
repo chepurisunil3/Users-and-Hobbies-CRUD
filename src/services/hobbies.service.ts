ed799519a14a1397723c64f96304c64cefaa9f07
chepurisunil3@gmail.com <chepurisunil3@gmail.com>
2021-08-07 20:22:31 +0530

    Services for users and hobbies

---
+++ src/services/hobbies.service.ts
@@ -0,0 +1,94 @@
+import IHobbies, { isIHobbies } from "../interfaces/ihobbies.interface";
+import Hobbies from "../models/hobbies.model";
+import User from "../models/user.model";
+import mongoose from 'mongoose';
+import { IError } from "../interfaces/ierror.interface";
+import IUser from "../interfaces/iuser.interface";
+
+export const findAllHobbiesForUser = async (userId:string): Promise<IHobbies[]|IError> => {
+    const user:IUser|null = await User.findById(userId).select({name:0,__v:0,_id:0})
+    if (user)
+    {
+        const populatedHobbies = await user.populate('hobbies').execPopulate();
+        const hobbies:IHobbies[] = populatedHobbies.hobbies as IHobbies[];
+        return hobbies;
+    }
+    else {
+        const error:IError = {name:"User Not Found!",message:"The given user was not found"}
+        return error;
+    }
+}
+
+export const findHobbyWithId = async (id:string): Promise<IHobbies|IError> => {
+    const hobby:IHobbies|null = await Hobbies.findById(id);
+    if(hobby != null)
+    {
+        return hobby;
+    }
+    else {
+        const error:IError = {name:"Hobby not found",message:"Cannot find the specified hobby"}
+        return error;
+    }
+   
+}
+
+export const addHobbyForUser = async (userId:string,hobby:IHobbies): Promise<IHobbies|IError> => {
+    try {
+        if (await User.findById(userId))
+        {
+            const hobbiesToSave:IHobbies = new Hobbies(hobby);
+            const err = hobbiesToSave.validateSync();
+            err instanceof mongoose.Error;
+            if(err)
+            {
+                const messageToShow:string = err.message.split(":")[1]?.replaceAll("enum","value");
+                const error:IError = {name:err.name,message:messageToShow};
+                return error;
+            }
+            await hobbiesToSave.save();
+            await User.findByIdAndUpdate(userId,{$addToSet:{hobbies:hobbiesToSave._id.toString()}})
+            return hobbiesToSave;
+        }
+        else {
+            const error:IError = {name:"Not Found!",message:"The user is not found"};
+            return error;
+        }
+        
+    }
+    catch(exception)
+    {
+        const error:IError = {name:"Not Found!",message:"The user is not found"};
+        return error;
+    }
+}
+export const updateHobby = async (hobbyId:string,hobby:IHobbies): Promise<IHobbies|IError> => {
+    try {
+        const updatedHobby:IHobbies|null = await Hobbies.findByIdAndUpdate(hobbyId, hobby,{new: true});
+        if(updatedHobby == null)
+        {
+            const err:IError = {message:"No Records to Update!",name:"No Records to Update"}
+            return err;
+        }
+        else {
+            return updatedHobby;
+        }
+        
+    }
+    catch(exception)
+    {
+        console.log(exception)
+        const err:IError = {message:exception.message,name:exception.message}
+        return err;
+    }
+}
+export const removeHobby = async (hobbyId:string): Promise<boolean> => {
+    try {
+        await Hobbies.findByIdAndDelete(hobbyId);
+        return true;
+    }
+    catch(exception)
+    {
+        console.log(exception)
+        return false;
+    }
+}
---
+++ src/services/user.service.ts
@@ -0,0 +1,72 @@
+import IUser from "../interfaces/iuser.interface";
+import User from "../models/user.model";
+import mongoose from 'mongoose';
+import { IError } from "../interfaces/ierror.interface";
+
+export const findAll = async (): Promise<IUser[]> => {
+    const users:IUser[] = await User.find().select({hobbies:0});
+    return users;
+}
+
+export const findUserWithId = async (id:string): Promise<IUser|null> => {
+    const user:IUser|null = await User.findById(id).select({hobbies:0});
+    return user;
+}
+
+export const addUser = async (user:IUser): Promise<IUser|IError> => {
+    try {
+        const userToSave:IUser = new User(user);
+        const err = userToSave.validateSync();
+        err instanceof mongoose.Error;
+        if(err)
+        {
+            const messageToShow:string = err.message.split(":")[1]?.replaceAll("enum","value");
+            const error:IError = {name:err.name,message:messageToShow};
+            return error;
+        }
+        await userToSave.save();
+        return userToSave;
+            
+    }
+    catch(exception)
+    {
+        const messageToShow:string = exception.message.split(":")[1]?.replaceAll("enum","value");
+        const error:IError = {name:exception.name,message:messageToShow};
+        return error;
+    }
+}
+export const updateUser = async (id:string,user:IUser): Promise<IUser|IError> => {
+    try {
+        if(!await User.findById(id))
+        {
+            const error:IError = {name:"Not Found!",message:"The user is not found"};
+            return error;
+        }
+        const updatedUser:IUser|null = await User.findByIdAndUpdate(id, user,{new:true});
+        if (updatedUser)
+            return updatedUser;
+        else
+        {
+            const error:IError = {name:"No records to Update!",message:"Cannot update the selected fields, Probably the details are already updated"};
+            return error;
+        }
+    }
+    catch(exception)
+    {
+        console.log(exception)
+        const messageToShow:string = exception.message.split(":")[1]?.replaceAll("enum","value");
+        const error:IError = {name:exception.name,message:messageToShow};
+        return error;
+    }
+}
+export const removeUser = async (id:string): Promise<boolean> => {
+    try {
+        await User.findByIdAndDelete(id);
+        return true;
+    }
+    catch(exception)
+    {
+        console.log(exception)
+        return false;
+    }
+}
