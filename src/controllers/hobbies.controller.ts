2279f0510397dbe213f925bc50267b224775aa54
chepurisunil3@gmail.com <chepurisunil3@gmail.com>
2021-08-07 20:22:13 +0530

    Controllers for users and hobbies

---
+++ src/controllers/hobbies.controller.ts
@@ -0,0 +1,65 @@
+import {Request,Response,NextFunction} from "express";
+import { IError, isIError } from "../interfaces/ierror.interface";
+import IHobbies, { isIHobbies } from "../interfaces/ihobbies.interface";
+import { addHobbyForUser, findAllHobbiesForUser, findHobbyWithId, removeHobby, updateHobby } from "../services/hobbies.service";
+
+export const getAllHobbiesForUser = async (req: Request,res: Response,next: NextFunction) => {
+    const userId:string = req.params.userId;
+    const hobbies:IHobbies[]|IError = await findAllHobbiesForUser(userId);
+    if(!isIError(hobbies))
+    {
+        return res.status(200).json(hobbies)
+    }
+    else {
+        return res.status(400).json({name:"User not found!",message:"Cannot find the user to retrieve the hobbies"})
+    }
+    
+}
+
+export const getSpecificHobbyForUser = async (req: Request,res: Response,next: NextFunction) => {
+    const id:string = req.params.id as string;
+    const hobby:IHobbies|IError = await findHobbyWithId(id);
+    if(!isIError(hobby))
+        return res.status(200).json(hobby)
+    else 
+        return res.status(400).json({success:false,message:"Cannot find the Hobby!"})
+}
+
+export const addNewHobby = async (req: Request,res: Response,next: NextFunction) => {
+    const hobby:IHobbies = req.body;
+    const userId:string = req.params.userId
+    const newHobby:IHobbies|IError = await addHobbyForUser(userId,hobby)
+    if(!isIError(newHobby))
+    {
+        return res.status(200).json(newHobby)
+    }
+    else {
+        return res.status(200).json({success:false,message:"Cannot add the Hobby! Probably required details not provided"})
+    }
+}
+
+export const updateExistingHobby = async (req: Request,res: Response,next: NextFunction) => {
+    const hobbyId:string = req.params.id;
+    const updateData:IHobbies = req.body;
+    const updatedHobby = await updateHobby(hobbyId,updateData)
+    if(!isIError(updatedHobby))
+    {
+        return res.status(200).json(updatedHobby)
+    }
+    else {
+        return res.status(200).json({success:false,message:"Cannot find the hobby"})
+    }
+    
+}
+
+export const deleteHobbyForUser = async (req: Request,res: Response,next: NextFunction) => {
+    const id:string = req.params.id as string;
+    const isDeleted:boolean = await removeHobby(id);
+    if(isDeleted)
+    {
+        return res.status(200).json({success:true})
+    }
+    else {
+        return res.status(400).json({name:"User not found",message:"Cannot find the User Specified"})
+    }
+}
---
+++ src/controllers/users.controller.ts
@@ -0,0 +1,52 @@
+import {Request,Response,NextFunction} from "express";
+import { IError, isIError } from "../interfaces/ierror.interface";
+import IUser, { isIUser } from "../interfaces/iuser.interface";
+import { addUser, findAll, findUserWithId, removeUser, updateUser } from "../services/user.service";
+
+export const getAllUsers = async (req: Request,res: Response,next: NextFunction) => {
+    const users:IUser[] = await findAll();
+    return res.status(200).json(users)
+}
+export const getSpecificUser = async (req: Request,res: Response,next: NextFunction) => {
+    const id:string = req.params.id as string;
+    const user:IUser|null = await findUserWithId(id);
+    if(user)
+        return res.status(200).json(user)
+    else 
+        return res.status(400).json({success:false,message:"Cannot find the user!"})
+}
+export const addNewUser = async (req: Request,res: Response,next: NextFunction) => {
+    const user:IUser = req.body;
+    const newUser:IUser|IError = await addUser(user)
+    if(!isIError(newUser))
+    {
+        return res.status(200).json(newUser)
+    }
+    else {
+        return res.status(400).json({name:newUser.name,message:newUser.message})
+    }
+    
+}
+export const updateExistingUser = async (req: Request,res: Response,next: NextFunction) => {
+    const userId:string = req.params.id;
+    const updateData:IUser = req.body;
+    const updatedUser:IUser|IError = await updateUser(userId,updateData);
+    if(!isIError(updatedUser))
+    {
+        return res.status(200).json(updatedUser)
+    }
+    else {
+        return res.status(400).json({name:"User not found",message:"Cannot find the User Specified"})
+    }
+}
+export const deleteUser = async (req: Request,res: Response,next: NextFunction) => {
+    const id:string = req.params.id as string;
+    const isDeleted:boolean = await removeUser(id);
+    if(isDeleted)
+    {
+        return res.status(200).json({success:true})
+    }
+    else {
+        return res.status(400).json({name:"User not found",message:"Cannot find the User Specified"})
+    }
+}
